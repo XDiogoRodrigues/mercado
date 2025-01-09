4ed001061300c6ea290f01278e1dba17e374f293
Diogo Rodrigues <diogosouzak15@gmail.com>
2025-01-09 13:31:18 -0300

    Modificação de tipos de variavel na classe Product, Declarada a classe ShoppingCard, com a property e um metodo add_item

--- classes.py
+++ classes.py
@@ -7,7 +7,7 @@ class Product:
 
     def __init__(self, name: str, price: float, quantity: int, is_in_stock: bool):
         self.__name: str = name.title()
-        self.__price: float = price
+        self.__price: float = float(price)
         self.__quantity: int = quantity
         self.__is_in_stock: bool = is_in_stock
     
@@ -16,7 +16,7 @@ class Product:
         return self.__name
     
     @name.setter
-    def name(self, new_name):
+    def name(self, new_name: str):
         self.__name = new_name
 
     @property
@@ -24,7 +24,7 @@ class Product:
         return self.__price
     
     @price.setter
-    def price(self, new_price):
+    def price(self, new_price: float):
         self.__price = new_price
     
     @property
@@ -32,7 +32,7 @@ class Product:
         return self.__quantity
     
     @quantity.setter
-    def quantity(self, new_quantity):
+    def quantity(self, new_quantity: int):
         self.__quantity = new_quantity
     
     @property
@@ -40,8 +40,32 @@ class Product:
         return self.__is_in_stock
 
     @is_in_stock.setter
-    def is_in_stock(self, stock):
+    def is_in_stock(self, stock: bool):
         self.__is_in_stock = stock
+
+
+class ShoppingCart:
+
+    def __init__(self):
+        self.__items: list = []
+
+    @property
+    def items(self):
+        return self.__items
     
-    def __str__(self):
-        return f'Nome: {self.name}\nPreço: {locale.currency(self.price)}'
+    @items.setter
+    def items(self, new_item: dict):
+        self.__items.append(new_item)
+                                          
+    def add_item(self, item_verification: Product):
+        in_list: bool = False
+        for item in self.items:
+            for chave, valor in item.items():
+                if chave == item_verification.name:
+                    item[chave][1] += 1
+                    in_list = True
+        if in_list == False:
+            produto: dict = {f'{item_verification.name}': [item_verification.price, 1]}
+            self.items = produto
+               
+       
